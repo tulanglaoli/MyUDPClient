43d8da77050d1dee9a1992b9c880a76f43fc50d0
tulanglaoli@foxmail.com <tulanglaoli@foxmail.com>
2015-05-25 19:04:25 +0800

    Init

---
+++ MyUDPClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+using AsyncClient;
+using AsyncServer;
+using System.Threading;
+
+namespace MyUDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //AsyncUdpSever.Servermain("192.168.1.103");
+            AsyncUdpClient.Init();
+            while(true)
+            {
+
+                AsyncUdpClient.auc.SendMsg(Console.ReadLine());
+            }
+
+
+        }
+    }
+}
